ed68cb2ea655d866dab9bc4cf92a889206afe80a
Jon Park <jon@jonathanpark.ca>
2023-01-11 16:06:06 -0500

    feat: update startship.zsh

--- .zsh/starship.zsh
+++ .zsh/starship.zsh
@@ -1,38 +1,36 @@
-# find out which distribution we are running on
-LFILE="/etc/*-release"
-MFILE="/System/Library/CoreServices/SystemVersion.plist"
-if [[ -f $LFILE ]]; then
-  _distro=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
-elif [[ -f $MFILE ]]; then
-  _distro="macos"
-fi
 
-# set an icon based on the distro
-# make sure your font is compatible with https://github.com/lukas-w/font-logos
-case $_distro in
-    *kali*)                  ICON="ﴣ";;
-    *arch*)                  ICON="";;
-    *debian*)                ICON="";;
-    *raspbian*)              ICON="";;
-    *ubuntu*)                ICON="";;
-    *elementary*)            ICON="";;
-    *fedora*)                ICON="";;
-    *coreos*)                ICON="";;
-    *gentoo*)                ICON="";;
-    *mageia*)                ICON="";;
-    *centos*)                ICON="";;
-    *opensuse*|*tumbleweed*) ICON="";;
-    *sabayon*)               ICON="";;
-    *slackware*)             ICON="";;
-    *linuxmint*)             ICON="";;
-    *alpine*)                ICON="";;
-    *aosc*)                  ICON="";;
-    *nixos*)                 ICON="";;
-    *devuan*)                ICON="";;
-    *manjaro*)               ICON="";;
-    *rhel*)                  ICON="";;
-    *macos*)                 ICON="";;
-    *)                       ICON="";;
-esac
+if [[ ${LINUX} ]]; then
+  # find out which distribution we are running on
+  distro=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
+
+  # set an icon based on the distro
+  case $distro in
+      *kali*)                  ICON="ﴣ";;
+      *arch*)                  ICON="";;
+      *debian*)                ICON="";;
+      *raspbian*)              ICON="";;
+      *ubuntu*)                ICON="";;
+      *elementary*)            ICON="";;
+      *fedora*)                ICON="";;
+      *coreos*)                ICON="";;
+      *gentoo*)                ICON="";;
+      *mageia*)                ICON="";;
+      *centos*)                ICON="";;
+      *opensuse*|*tumbleweed*) ICON="";;
+      *sabayon*)               ICON="";;
+      *slackware*)             ICON="";;
+      *linuxmint*)             ICON="";;
+      *alpine*)                ICON="";;
+      *aosc*)                  ICON="";;
+      *nixos*)                 ICON="";;
+      *devuan*)                ICON="";;
+      *manjaro*)               ICON="";;
+      *rhel*)                  ICON="";;
+      *)                       ICON="";;
+  esac
+fi
+if [[ ${MACOS} ]]; then
+  ICON=""
+fi
 
 export STARSHIP_DISTRO="$ICON"
